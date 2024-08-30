d1c4511ca73a0385f9cf6d246366991ccbc50e84
Daniel Katz <danielka1010@gmail.com>
2024-08-30 13:21:23 +0300

    Refactor GarageRegistry : add gets

--- Ex03.GarageLogic/GarageRegistry.cs
+++ Ex03.GarageLogic/GarageRegistry.cs
@@ -5,6 +5,26 @@ public class GarageRegistry
     private readonly Vehicle r_RegisteredVehcle;
     private eVehicleStatus m_VehicleStatus;
 
+    public Vehicle RegisteredVehcle
+    {
+        get
+        {
+            return r_RegisteredVehcle;
+        }
+    }
+    
+    public eVehicleStatus VehicleStatus
+    {
+        get
+        {
+            return m_VehicleStatus;
+        }
+        set
+        {
+            m_VehicleStatus = value;
+        }
+    }
+
     public GarageRegistry(Vehicle iRegisteredVehcle, eVehicleStatus iVehicleStatus)
     {
         r_RegisteredVehcle = iRegisteredVehcle;
