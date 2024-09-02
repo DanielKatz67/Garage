086efbc22546f7ce4a56c9c37e5ff1dc6155bbf2
Daniel Katz <danielka1010@gmail.com>
2024-09-02 12:05:10 +0300

    Sent const values instead of input in the Truck ctor

--- Ex03.GarageLogic/Truck.cs
+++ Ex03.GarageLogic/Truck.cs
@@ -9,14 +9,13 @@ public class Truck : Vehicle
     private const float k_MaximalFuelTankCapacity = 130.0F;
     private const int k_WheelsNumberInTruck = 14;
 
-    public Truck(string i_LicensePlate, string i_ModelName, string i_WheelsManufacturer, 
-        bool i_IsCarryingDangerousMaterials, float i_TrunkCargoVolume, 
-        eEnergySourceType i_EnergySourceType, float i_CurrentEnergySourceCapacity,
-        eFuelType i_FuelType, float? i_MaximalFuelTankCapacity, VehicleOwner i_VehicleOwner)
+    public Truck(string i_LicensePlate, string i_ModelName, string i_WheelsManufacturer,
+            float i_CurrentWheelsAirPressure, bool i_IsCarryingDangerousMaterials, float i_TrunkCargoVolume, 
+            eEnergySourceType i_EnergySourceType, float i_CurrentEnergySourceCapacity, VehicleOwner i_VehicleOwner)
         : base(i_LicensePlate, k_WheelsNumberInTruck, i_ModelName, i_WheelsManufacturer, 
-            k_MaximumWheelsAirPressure, i_CurrentEnergySourceCapacity,
-            i_EnergySourceType, i_CurrentEnergySourceCapacity, i_FuelType, 
-            i_MaximalFuelTankCapacity, null, i_VehicleOwner)
+            k_MaximumWheelsAirPressure, i_CurrentWheelsAirPressure,
+            i_EnergySourceType, i_CurrentEnergySourceCapacity, k_FuelType, 
+            k_MaximalFuelTankCapacity, null, i_VehicleOwner)
     {
         m_IsCarryingDangerousMaterials = i_IsCarryingDangerousMaterials;
         m_TrunkCargoVolume = i_TrunkCargoVolume;
